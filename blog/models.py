1f1b265ea99f08ca19c91228c5a9a9dee6ed55fd
Deissei <privetizkyrgyzstana2@gmail.com>
2023-10-09 16:55:58 +0600

    add admin panel

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,9 @@ class Category(models.Model):
     title = models.CharField(
         max_length=255,
     )
+    
+    def __str__(self):
+        return f"{self.title}"
 
 
 class Post(models.Model):
