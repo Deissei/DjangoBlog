7b31bb6f608dec860df88599a25601c9e202c0da
Deissei <privetizkyrgyzstana2@gmail.com>
2023-10-09 17:38:34 +0600

    add logics create post view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Category, Post
 
-# Create your views here.
+def create_post(request):
+    if request.method == "POST":
+        category = Category.objects.get(id=request.POST["category"])
+        title = request.POST["title"]
+        description = request.POST["description"]
+        author = request.POST["author"]
+        
+        Post.objects.create(
+            category=category,
+            title=title,
+            description=description,
+            author=author,
+        )
+        return redirect("/")
+    
+    if request.method == "GET":
+        category = Category.objects.all()
+        context = {
+            "category": category,
+        }
+    
+    return render(request, "create_post.html", context)
