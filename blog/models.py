c528d152ab563b43c704ee1186898c4b06c56fea
Deissei <privetizkyrgyzstana2@gmail.com>
2023-10-09 16:44:55 +0600

    add model category, post and add create_post.html not logics

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """ Модель(Таблица Категории)
+    """
+    title = models.CharField(
+        max_length=255,
+    )
+
+
+class Post(models.Model):
+    """ Модель(Таблица Поста)
+    """
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(
+        max_length=255,
+    )
+    description = models.TextField(
+        max_length=2000,
+    )
+    created_at = models.DateField(
+        auto_now_add=True,
+    )
+    author = models.CharField(
+        max_length=30,
+    )
+    
