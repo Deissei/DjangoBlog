050f017d825dea9ca3dc85097d08cd838af71bdc
Deissei <privetizkyrgyzstana2@gmail.com>
2023-10-09 17:40:51 +0600

    homework

--- blog/urls.py
+++ blog/urls.py
@@ -4,3 +4,8 @@ from .views import create_post
 urlpatterns = [
     path('create_post/', create_post, name="create_post"),
 ]
+
+
+# Создать функцию которая показывает index.html
+
+# Добавишь путь что бы пользователь после захода в главную странцу показывался index.html
